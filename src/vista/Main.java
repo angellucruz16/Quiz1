f53107b9dcc33dddc3e619a4293e0f3a381f4434
angellucruz16 <angellucruz16@hotmail.com>
2020-10-09 18:12:57 -0500

    COMMIT 1
    Cree los paquetes y la clase Main

---
+++ src/vista/Main.java
@@ -0,0 +1,48 @@
+package vista;
+
+import processing.core.PApplet;
+public class Main extends PApplet {	
+	
+	//VARIABLES
+	
+	int posX;
+	int posY;
+	int dir;
+	int tam;
+		
+	
+	public void settings () {
+		size (1328,830);
+		                               
+	} // SETTINGS
+
+	public void setup () {
+	
+			
+		
+	} //SETUP
+	
+	public void draw () {
+		//  
+		
+		
+	}// DRAW 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+} //MAIN
